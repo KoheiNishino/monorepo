6335d56dd36b782e75329383e3d8f4ca3eb944b8
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2025-10-25 11:46:00 +0900

    setup eslint and tsconfig

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,26 @@
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
 export default defineConfig(
+	includeIgnoreFile(gitignorePath),
 	eslint.configs.recommended,
 	tseslint.configs.strictTypeChecked,
 	tseslint.configs.stylisticTypeChecked,
 	reactHooks.configs.flat.recommended,
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
 );
