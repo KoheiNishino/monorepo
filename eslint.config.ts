151163aa364d74b852293300410ff52ca08fda57
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2025-10-26 11:37:02 +0900

    update eslint.config tsconfig

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,25 @@
 import { fileURLToPath } from "node:url";
 import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
 const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig(
+	globalIgnores(["eslint.config.ts"]),
 	includeIgnoreFile(gitignorePath),
 	eslint.configs.recommended,
 	tseslint.configs.strictTypeChecked,
 	tseslint.configs.stylisticTypeChecked,
-	reactHooks.configs.flat.recommended,
+	reactHooks.configs.flat.recommended!,
 	{
 		files: ["**/*.ts", "**/*.tsx"],
+		rules: {
+			"@typescript-eslint/consistent-type-imports": "error",
+			"@typescript-eslint/no-import-type-side-effects": "error",
+		},
 		languageOptions: {
 			parser: tseslint.parser,
 			parserOptions: {
