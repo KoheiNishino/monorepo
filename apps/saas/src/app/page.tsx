1c36b4683bdc77e6b15d8db57cd7206b3e47c7d0
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2025-11-03 22:28:42 +0900

    fix(page): remove background color from Home component

--- apps/saas/src/app/page.tsx
+++ apps/saas/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div
 			className={`
-				flex min-h-screen items-center justify-center bg-zinc-50 font-sans
+				flex min-h-screen items-center justify-center font-sans
 				dark:bg-black
 			`}
 		>
