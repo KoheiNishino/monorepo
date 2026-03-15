2af532ee41e9f5d4c72a62abac194850486988a8
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2026-03-15 09:52:52 +0900

    pnpm workspace

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+import {
+  createContext,
+  use,
+  useEffect,
+  useReducer,
+  useState,
+  type ComponentProps,
+  type FC,
+} from "react";
+
+type Todo = {
+  id: string;
+  title: string;
+  isDone: boolean;
+};
+
+type Action =
+  | {
+      type: "add";
+      title: Todo["title"];
+    }
+  | {
+      type: "check";
+      id: Todo["id"];
+    }
+  | {
+      type: "delete";
+      id: Todo["id"];
+    };
+
+const reducer = (state: Todo[], action: Action) => {
+  if (action.type === "add") {
+    const newTodos = [
+      ...state,
+      {
+        id: crypto.randomUUID(),
+        title: action.title,
+        isDone: false,
+      },
+    ];
+    return newTodos;
+  } else if (action.type === "delete") {
+    const newTodos = state.filter((t) => t.id !== action.id);
+    return newTodos;
+  } else if (action.type === "check") {
+    const newTodos = state.map((t) => (t.id === action.id ? { ...t, isDone: !t.isDone } : t));
+    return newTodos;
+  } else {
+    throw new Error("No action type is found");
+  }
+};
+
+const TodoContext = createContext<{
+  title: Todo["title"];
+  todos: Todo[];
+  addTodo: () => void;
+  checkTodo: (id: Todo["id"]) => void;
+  deleteTodo: (id: Todo["id"]) => void;
+  handleTitleChange: ComponentProps<"input">["onChange"];
+}>({
+  title: "",
+  todos: [],
+  addTodo: () => {},
+  checkTodo: () => {},
+  deleteTodo: () => {},
+  handleTitleChange: () => {},
+});
+
+const Todos: FC = () => {
+  const { title, todos, addTodo, checkTodo, deleteTodo, handleTitleChange } = use(TodoContext);
+
+  return (
+    <div css={{ display: "grid", gap: 16 }}>
+      <div css={{ display: "flex", gap: 8 }}>
+        <input onChange={handleTitleChange} value={title} />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <div css={{ display: "grid", gap: 8 }}>
+        {todos.map((todo) => (
+          <div key={todo.id} css={{ display: "flex", gap: 4 }}>
+            <input
+              type="checkbox"
+              checked={todo.isDone}
+              onChange={() => {
+                checkTodo(todo.id);
+              }}
+            />
+            <span>{todo.title}</span>
+            <button
+              css={{ color: "white", background: "red", borderColor: "red", borderRadius: 8 }}
+              onClick={() => {
+                deleteTodo(todo.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  const [title, setTitle] = useState("");
+  const [todos, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem("todos") ?? "[]"));
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoContext
+      value={{
+        title,
+        todos,
+        addTodo: () => {
+          dispatch({ type: "add", title });
+          setTitle("");
+        },
+        checkTodo: (id) => dispatch({ type: "check", id }),
+        deleteTodo: (id) => dispatch({ type: "delete", id }),
+        handleTitleChange: (e) => setTitle(e.target.value),
+      }}
+    >
+      <Todos />
+    </TodoContext>
+  );
+}
