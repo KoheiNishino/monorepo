57678adc3f0cb50ecf6e9cebc040e0fe3d257a75
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2026-03-29 08:37:55 +0900

    fetch

---
+++ apps/fetch/src/App.tsx
@@ -0,0 +1,54 @@
+import { Suspense, use } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface Todo {
+	userId: number;
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+const cache = new Map<string, Promise<Todo[]>>();
+async function fetchTodos(url: string) {
+	if (!cache.has(url)) {
+		const res = await fetch(url);
+		if (!res.ok) {
+			throw Error("fetch is failed");
+		}
+		const todosJsonPromise = res.json() as Promise<Todo[]>;
+		cache.set(url, todosJsonPromise);
+		return todosJsonPromise;
+	}
+	return cache.get(url)!;
+}
+
+function Todos({ todosPromise }: { todosPromise: Promise<Todo[]> }) {
+	const todos = use(todosPromise);
+
+	return (
+		<div css={{ display: "grid", gap: 8 }}>
+			{todos.map(({ id, title, completed }) => {
+				return (
+					<div key={id} css={{ display: "flex", gap: 4 }}>
+						<input checked={completed} readOnly type="checkbox" />
+						<span>{title}</span>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+export default function App() {
+	return (
+		<ErrorBoundary fallback={<div>Something went wrong</div>}>
+			<Suspense fallback={<span>Loading...</span>}>
+				<Todos
+					todosPromise={fetchTodos(
+						"https://jsonplaceholder.typicode.com/todos",
+					)}
+				/>
+			</Suspense>
+		</ErrorBoundary>
+	);
+}
