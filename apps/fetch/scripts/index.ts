4c9396fe07addab88432d1f2d530312a04a950d0
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2026-04-05 11:04:26 +0900

    add error handling

--- apps/fetch/scripts/index.ts
+++ apps/fetch/scripts/index.ts
@@ -3,10 +3,22 @@ const ids = ["1", "2", "3"] as const;
 
 async function fetchSequential() {
 	console.info(fetchSequential.name);
+
 	for (const id of ids) {
-		const res = await fetch(`${apiUrl}/${id}`);
-		const data = (await res.json()) as unknown;
-		console.log(data);
+		try {
+			const res = await fetch(`${apiUrl}/${id}`);
+
+			if (!res.ok) {
+				throw new Error(`HTTP error! status: ${res.status.toString()}`);
+			}
+
+			const data = (await res.json()) as unknown;
+			console.log(data);
+		} catch (e) {
+			if (e instanceof Error) {
+				console.error("Fetch error:", e.message);
+			}
+		}
 	}
 }
 
@@ -14,9 +26,19 @@ async function fetchParallel() {
 	console.info(fetchParallel.name);
 	await Promise.all(
 		ids.map(async (id) => {
-			const res = await fetch(`${apiUrl}/${id}`);
-			const data = (await res.json()) as unknown;
-			console.log(data);
+			try {
+				const res = await fetch(`${apiUrl}/${id}`);
+
+				if (!res.ok) {
+					throw new Error(`HTTP error! status: ${res.status.toString()}`);
+				}
+				const data = (await res.json()) as unknown;
+				console.log(data);
+			} catch (e) {
+				if (e instanceof Error) {
+					console.error("Fetch error:", e.message);
+				}
+			}
 		}),
 	);
 }
