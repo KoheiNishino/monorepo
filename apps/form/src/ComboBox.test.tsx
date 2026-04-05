44f81d003497f27d5b0cb678c4098af02a803366
Kohei Nishino <58029195+KoheiNishino@users.noreply.github.com>
2026-04-05 10:09:24 +0900

    fix ComboBox test

--- apps/form/src/ComboBox.test.tsx
+++ apps/form/src/ComboBox.test.tsx
@@ -2,7 +2,26 @@ import { expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import App from "./App";
 
+const options = ["東京", "大阪"] as const;
+const label = "東京";
+
 test("ComboBox", async () => {
 	const screen = await render(<App />);
-	expect(screen.getByText("ホゲに変えるボタン")).toBeVisible();
+	const comboBox = screen.getByRole("textbox", { name: /^option$/ });
+
+	await comboBox.click();
+	for (const o of options) {
+		expect(screen.getByText(o)).toBeVisible();
+	}
+
+	// 選択肢が存在しないとき
+	await comboBox.fill("名古屋");
+	expect(
+		screen.getByText("一致する選択肢が見つかりませんでした"),
+	).toBeVisible();
+
+	// onChangeが発火する
+	await comboBox.fill(label);
+	await screen.getByText(label).click();
+	expect(comboBox).toHaveValue(label);
 });
